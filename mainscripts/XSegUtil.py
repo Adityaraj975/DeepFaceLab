ada272b92aac27f9393c6036f3da02ed8d24048f
Adityaraj975 <51711309+Adityaraj975@users.noreply.github.com>
2021-06-23 15:55:20 +0530

    Update XSegUtil.py

--- mainscripts/XSegUtil.py
+++ mainscripts/XSegUtil.py
@@ -2,7 +2,11 @@ import json
 import shutil
 import traceback
 from pathlib import Path
+from PIL import Image
+import dlib
+import cv2
 import numpy as np
+import time 
 from core import pathex
 from core.cv2ex import *
 from core.interact import interact as io
@@ -10,7 +14,6 @@ from core.leras import nn
 from DFLIMG import *
 from facelib import XSegNet, LandmarksProcessor, FaceType
 import pickle
-#import time
 
 def apply_xseg(input_path, model_path):
     if not input_path.exists():
@@ -18,7 +21,7 @@ def apply_xseg(input_path, model_path):
 
     if not model_path.exists():
         raise ValueError(f'{model_path} not found. Please ensure it exists.')
-        
+    
     face_type = None
     
     model_dat = model_path / 'XSeg_data.dat'
@@ -49,23 +52,31 @@ def apply_xseg(input_path, model_path):
         
     
     
-    xseg = XSegNet(name='XSeg', 
-                    load_weights=True,
-                    weights_file_root=model_path,
-                    data_format=nn.data_format,
-                    raise_on_no_model_files=True)
-    xseg_res = xseg.get_resolution()
+    #xseg = XSegNet(name='XSeg', 
+    #                load_weights=True,
+    #                weights_file_root=model_path,
+    #                data_format=nn.data_format,
+    #                raise_on_no_model_files=True)
+    xseg_res = 256
               
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
+    face_detector = dlib.get_frontal_face_detector()
+    #http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    file_path = "DeepFaceLab/data/Xseg/shape_predictor_81_face_landmarks.dat"
+    #file_path = "E:\DeepFaceLab_DirectX12 testing 1006\_internal\DeepFaceLab\data\Xseg\shape_predictor_81_face_landmarks.dat"
+    landmark_detector = dlib.shape_predictor(file_path)
+    
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
+        #t0 = time.time()          
+
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
             io.log_info(f'{filepath} is not a DFLIMG')
             continue
-        
+        imgg = cv2_imread(filepath)
         img = cv2_imread(filepath).astype(np.float32) / 255.0
         h,w,c = img.shape
-        
+    
         img_face_type = FaceType.fromString( dflimg.get_face_type() )
         if face_type is not None and img_face_type != face_type:
             lmrks = dflimg.get_source_landmarks()
@@ -85,15 +96,67 @@ def apply_xseg(input_path, model_path):
                 img = cv2.resize( img, (xseg_res,xseg_res), interpolation=cv2.INTER_LANCZOS4 )    
                     
         if len(img.shape) == 2:
-            img = img[...,None]            
-    
-        #t0 = time.time()
-        mask = xseg.extract(img)
-        #print(time.time()-t0)
+            img = img[...,None]     
+
+         
+        #imgg = dlib.load_rgb_image(filepath)
+        imgg = cv2.cvtColor(imgg, cv2.COLOR_BGR2RGB)
+
+        faces = face_detector(imgg, 1)
+        landmark_tuple = []
+        for k, d in enumerate(faces):
+           landmarks = landmark_detector(imgg, d)
+           for n in range(0, 81):
+              x = landmarks.part(n).x
+              y = landmarks.part(n).y
+              landmark_tuple.append((x, y))
+              #cv2.circle(img, (x, y), 2, (255, 255, 0), -1)
+        routes = []
+        for i in range(15, -1, -1):
+            from_coordinate = landmark_tuple[i+1]
+            to_coordinate = landmark_tuple[i]
+            routes.append(from_coordinate)
+
+        from_coordinate = landmark_tuple[0]
+        to_coordinate = landmark_tuple[17]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[75]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[76]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[68]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[69]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[72]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[73]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[79]
+        routes.append(from_coordinate)
+        from_coordinate = landmark_tuple[74]
+        routes.append(from_coordinate)
+        #from_coordinate = landmark_tuple[78]
+        #routes.append(from_coordinate)
+        to_coordinate = landmark_tuple[16]
+        routes.append(to_coordinate)
+        for i in range(0, len(routes)-1):
+            from_coordinate = routes[i]
+            to_coordinate = routes[i+1]
+            #img = cv2.line(img, from_coordinate, to_coordinate, (255, 255, 0), 1)
+        mask = np.zeros((img.shape[0], img.shape[1]))
+        mask = cv2.fillConvexPoly(mask, np.array(routes), 1)
+        mask = mask.astype('float32')
+        #t1 = time.time() - t0
+        #print("Time elapsed : ", t1)
+
         if face_type is not None and img_face_type != face_type:
             mask = cv2.resize(mask, (w, w), interpolation=cv2.INTER_LANCZOS4)
             mask = cv2.warpAffine( mask, mat, (w,w), np.zeros( (h,w,c), dtype=np.float), cv2.WARP_INVERSE_MAP | cv2.INTER_LANCZOS4)
             mask = cv2.resize(mask, (xseg_res, xseg_res), interpolation=cv2.INTER_LANCZOS4)
+        else :
+            mask = cv2.resize(mask, (xseg_res, xseg_res), interpolation=cv2.INTER_LANCZOS4)
+        
         mask[mask < 0.5]=0
         mask[mask >= 0.5]=1    
         dflimg.set_xseg_mask(mask)
